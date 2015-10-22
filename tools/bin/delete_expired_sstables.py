6100d2a45e2697f63bac1052a73705511bc0d2c7
mck <mck@apache.org>
2015-10-22 20:43:11 +1100

    SQUASH ME  simple python script to move sstables passed in via stdin

---
+++ tools/bin/delete_expired_sstables.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#
+
+import csv
+import glob
+import os, sys
+
+backups = 'fully-expired-backups'
+data = csv.reader(sys.stdin)
+for row in data:
+    if not ''.join(row).strip():
+        pass
+    else:
+        sstable = row[1]
+        print("deleting " + sstable)
+        for f in glob.glob(sstable + "*"):
+            #os.rename(f, os.path.dirname(f) + "/" + backups + "/" + os.path.basename(f))
+            print("  (moved " + os.path.basename(f) + " into " + os.path.dirname(f) + backups + "/)")
