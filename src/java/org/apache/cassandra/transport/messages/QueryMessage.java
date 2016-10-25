f9e2db326296cbb405d879700c73acea3e8d3fdb
Mick Semb Wever <mck@apache.org>
2016-10-25 20:00:46 +1100

    Tracing payload not passed from QueryMessage to tracing session

--- src/java/org/apache/cassandra/transport/messages/QueryMessage.java
+++ src/java/org/apache/cassandra/transport/messages/QueryMessage.java
@@ -98,7 +98,7 @@ public class QueryMessage extends Message.Request
 
             if (state.traceNextQuery())
             {
-                state.createTracingSession();
+                state.createTracingSession(getCustomPayload());
 
                 ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
                 builder.put("query", query);
