1d48cc3c557a88a80f67b664ad48fb3a871decea
Stefan Podkowinski <stefan.podkowinski@1und1.de>
2018-05-23 15:06:29 +0200

    Add first take on diag event persistence

---
+++ src/java/org/apache/cassandra/diag/DiagnosticEventPersistence.java
@@ -0,0 +1,200 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.diag;
+
+import java.io.InvalidClassException;
+import java.io.Serializable;
+import java.lang.management.ManagementFactory;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.cassandra.diag.store.DiagnosticEventMemoryStore;
+import org.apache.cassandra.diag.store.DiagnosticEventStore;
+import org.apache.cassandra.jmx.AbstractEventIdCollector;
+
+
+/**
+ * Manages storing and retrieving events based on enabled {@link DiagnosticEventStore} implementation.
+ */
+public class DiagnosticEventPersistence implements DiagnosticEventPersistenceMBean
+{
+    private static final Logger logger = LoggerFactory.getLogger(DiagnosticEventPersistence.class);
+
+    private static DiagnosticEventPersistence instance;
+
+    private final Map<Class, DiagnosticEventStore<Long>> stores = new ConcurrentHashMap<>();
+
+    private final DiagnosticEventIdCollector lastIdCollector = new DiagnosticEventIdCollector();
+
+    private final Consumer<DiagnosticEvent> eventConsumer = this::onEvent;
+
+    private DiagnosticEventPersistence()
+    {
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        try
+        {
+            ObjectName jmxObjectName = new ObjectName("org.apache.cassandra.diag:type=DiagnosticEventPersistence");
+            mbs.registerMBean(this, jmxObjectName);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void start()
+    {
+        instance = new DiagnosticEventPersistence();
+    }
+
+    public DiagnosticEventPersistence instance()
+    {
+        return instance;
+    }
+
+    @Override
+    public SortedMap<Long, Map<String, Serializable>> getEvents(String eventClazz, Long key, int limit, boolean includeKey)
+    {
+        assert eventClazz != null;
+        assert key != null;
+        assert limit >= 0;
+
+        Class cls;
+        try
+        {
+            cls = getEventClass(eventClazz);
+        }
+        catch (ClassNotFoundException | InvalidClassException e)
+        {
+            throw new RuntimeException(e);
+        }
+        DiagnosticEventStore<Long> store = getStore(cls);
+
+        NavigableMap<Long, DiagnosticEvent> events = store.scan(key, includeKey ? limit : limit + 1);
+        if (!includeKey && events.size() > 1) events.pollFirstEntry();
+
+        TreeMap<Long, Map<String, Serializable>> ret = new TreeMap<>();
+        for (Map.Entry<Long, DiagnosticEvent> entry : events.entrySet())
+        {
+            DiagnosticEvent event = entry.getValue();
+            HashMap<String, Serializable> val = new HashMap<>(event.toMap());
+            val.put("class", event.getClass().getName());
+            val.put("type", event.getType().name());
+            val.put("ts", event.timestamp);
+            ret.put(entry.getKey(), val);
+        }
+        logger.debug("Returning {} events for key {} (limit {}) (includeKey {})", ret.size(), key, limit, includeKey);
+        return ret;
+    }
+
+    @Override
+    public void enableEventPersistence(String eventClazz)
+    {
+        try
+        {
+            logger.debug("Enabling events: {}", eventClazz);
+            DiagnosticEventService.instance().subscribe(getEventClass(eventClazz), eventConsumer);
+        }
+        catch (ClassNotFoundException | InvalidClassException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void disableEventPersistence(String eventClazz)
+    {
+        try
+        {
+            logger.debug("Disabling events: {}", eventClazz);
+            DiagnosticEventService.instance().unsubscribe(getEventClass(eventClazz), this::onEvent);
+        }
+        catch (ClassNotFoundException | InvalidClassException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void onEvent(DiagnosticEvent event)
+    {
+        Class<? extends DiagnosticEvent> cls = event.getClass();
+        if (logger.isTraceEnabled())
+            logger.trace("Persisting received {} event", cls.getName());
+        DiagnosticEventStore<Long> store = getStore(cls);
+        store.store(event);
+        lastIdCollector.onEvent(new PersistedDiagnosticEvent(store.getLastEventId(), event));
+    }
+
+    private Class<DiagnosticEvent> getEventClass(String eventClazz) throws ClassNotFoundException, InvalidClassException
+    {
+        // get class by eventClazz argument name
+        // restrict class loading for security reasons
+        if (!eventClazz.startsWith("org.apache.cassandra."))
+            throw new RuntimeException("Not a Cassandra event class");
+
+        Class<DiagnosticEvent> clazz = (Class<DiagnosticEvent>) Class.forName(eventClazz);
+
+        if (!(DiagnosticEvent.class.isAssignableFrom(clazz)))
+            throw new InvalidClassException("Event class must be of type DiagnosticEvent");
+
+        return clazz;
+    }
+
+    private DiagnosticEventStore<Long> getStore(Class cls)
+    {
+        return stores.computeIfAbsent(cls, (storeKey) -> new DiagnosticEventMemoryStore());
+    }
+
+    private static class PersistedDiagnosticEvent
+    {
+        Long key;
+        DiagnosticEvent event;
+
+        PersistedDiagnosticEvent(Long key, DiagnosticEvent event)
+        {
+            this.key = key;
+            this.event = event;
+        }
+    }
+
+    private static class DiagnosticEventIdCollector extends AbstractEventIdCollector<PersistedDiagnosticEvent, String, Long>
+    {
+        @Override
+        public Long eventId(PersistedDiagnosticEvent event)
+        {
+            return event.key;
+        }
+
+        @Override
+        public String eventType(PersistedDiagnosticEvent event)
+        {
+            return event.event.getClass().getName();
+        }
+    }
+}
