ac8cccfde0fb8f61d86a063ffb5ec590feb7ad11
polandll <lorinapoland@gmail.com>
2020-08-07 12:07:48 -0700

    saving to switch

--- doc/docker-entrypoint.sh
+++ doc/docker-entrypoint.sh
@@ -10,8 +10,7 @@ set -e
 # trunk will be the initial branch
 cd ${BUILD_DIR}/cassandra && git checkout doc_redo_asciidoc; 
 ant jar;
-cd doc && python3 gen-nodetool-docs.py; 
-#python3 convert_yaml_to_adoc.py;
+cd doc && python3 gen-nodetool-docs.py && python3 convert_yaml_to_adoc.py;
 
 # and clean to get ready for the next branch
 #ant realclean;
