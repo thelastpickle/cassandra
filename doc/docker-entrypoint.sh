dc3a120b3f5a25e2e05342982f6599152a319c5d
polandll <lorinapoland@gmail.com>
2020-08-11 17:33:52 -0700

    add add commit

--- doc/docker-entrypoint.sh
+++ doc/docker-entrypoint.sh
@@ -41,9 +41,13 @@ do
   YAML_OUTPUT="${BUILD_DIR}"/cassandra/doc/source/modules/cassandra/pages/configuration/cass_yaml_file.adoc
   #YAML_OUTPUT=doc/build_gen/"${branch_name}"/cass_yaml_file.adoc
   python3 convert_yaml_to_adoc.py ${YAML_INPUT} ${YAML_OUTPUT}
+
+  # need to add,commit changes
+  git add && git commit -m "${branch_name} nodetool changes
   #mkdir -p doc/build_gen/"${branch_name}"/
   #mv source/modules/cassandra/pages/tools/nodetool/ doc/build_gen/"${branch_name}"/
   #mv source/modules/cassandra/pages/configuration/cass_yaml_file.adoc doc/build_gen/"${branch_name}"/
+
   # After copy we will have:
   # ACTUALLY HAVE doc/doc/build_gen/<branch_name>
   # doc/build_gen/<branch_name>
