10db4145e1997a1e2e9cc92814dd239cc765921a
polandll <lorinapoland@gmail.com>
2020-08-11 18:56:51 -0700

    more changes

--- doc/docker-entrypoint.sh
+++ doc/docker-entrypoint.sh
@@ -5,13 +5,16 @@ set -e
 
 # -Check if we have an argument for a reference repository
 # -If we have a reference repository, then copy changes to branch that is changed (optional)
-# Run python scripts to generate nodetool output and cassandra.yaml
+# -Run python scripts to generate nodetool output and cassandra.yaml
 # Run antora
 
 USE_REPOSITORY="${1}"
+export CASSANDRA_USE_JDK11=true
 
 # @TODO: Consider changing the argument to be a path to a repository rather than a flag??
-if [ "${USE_REPOSITORY}" = "true" ]
+
+echo "If this is your first time running this container script, go get a coffee - it's going to take awhile."
+if [ "${USE_REPOSITORY}" = "preview" ]
 then
   cd "${REF_DIR}"/cassandra
   ref_branch=$(git branch | grep "\*" | cut -d' ' -f2)
@@ -31,20 +34,26 @@ fi
 BRANCH_LIST="doc_redo_asciidoc doc_redo_asciidoc3.11"
 
 cd "${BUILD_DIR}"/cassandra
+
+echo "checking out branch"
 for branch_name in ${BRANCH_LIST}
 do
-  export CASSANDRA_USE_JDK11=true
+  git checkout "${branch_name}"
+  echo "building jar files"
   ant jar
   cd doc
-  git checkout "${branch_name}"
+  # generate the nodetool docs
+  echo "generate nodetool"
   python3 gen-nodetool-docs.py
+  # generate cassandra.yaml doc file
+  echo "generate cassandra.yaml"
   YAML_INPUT="${BUILD_DIR}"/cassandra/conf/cassandra.yaml
   YAML_OUTPUT="${BUILD_DIR}"/cassandra/doc/source/modules/cassandra/pages/configuration/cass_yaml_file.adoc
   python3 convert_yaml_to_adoc.py ${YAML_INPUT} ${YAML_OUTPUT}
 
   # need to add,commit changes before changing branches
-  git add && git commit -m "${branch_name} nodetool changes"
-
+  git add . && git commit -m "${branch_name} nodetool changes"
+  echo "clean up"
   ant realclean
 done
 
@@ -57,18 +66,3 @@ ANTORACMD  = antora
 DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr DOCSEARCH_INDEX_VERSION=latest antora site.yml
 #${ANTORAOPTS} ${ANTORACMD} ${ANTORAYAML}
 
-# DOING IN DOCKERFILE
-#git clone https://gitbox.apache.org/repos/asf/cassandra.git ${BUILD_DIR}/cassandra && \
-#chmod -R a+rw ${BUILD_DIR}
-
-# trunk will be the initial branch
-#cd ${BUILD_DIR}/cassandra && git checkout doc_redo_asciidoc;
-#ant jar;
-#cd doc && python3 gen-nodetool-docs.py && python3 convert_yaml_to_adoc.py;
-
-# and clean to get ready for the next branch
-#ant realclean;
-
-#cd ${BUILD_DIR}/cassandra && git checkout doc_redo_asciidoc3.11;
-#ant jar;
-#cd doc; python3 gen-nodetool-docs.py; python3 convert_yaml_to_adoc.py;
