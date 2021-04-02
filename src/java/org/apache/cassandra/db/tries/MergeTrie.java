29adbbb397c320a3187cebf5084380102627dd92
Branimir Lambov <branimir.lambov@datastax.com>
2021-04-02 12:57:19 +0300

    STAR-508: Switch Trie to Cursor representation
    This replaces the Node-based walks and transformations. The result is
    drastically less intermediate object creation, improved performance
    and somewhat simpler code at the expense of the concept being a little
    harder to understand initially.
    
    Adds further documentation and expands tests for sliced tries.
    
    (cherry picked from commit 2b3c4c50d65a7ae93f0ee9bc9848064ff03733d2)
    (cherry picked from commit 43c520617fe46b92d4cba58eae8365777082b5bb)
    (cherry picked from commit 40dcfd51aec524dfbfad23c0cf60472a09bae4ce)
    (cherry picked from commit d3436c4d95860a60ff34e5936fd6eb2060946e7e)
    (cherry picked from commit 96d876b312fad9081ff4bce1a84db5f47d5b6806)
    (cherry picked from commit 1605a5a8c2da238f02424e1314195ef1a6f8cf99)

--- src/java/org/apache/cassandra/db/tries/MergeTrie.java
+++ src/java/org/apache/cassandra/db/tries/MergeTrie.java
@@ -118,6 +118,7 @@ class MergeTrie<T> extends Trie<T>
             int c2trans = c2.incomingTransition();
             atC1 = c1trans <= c2trans;
             atC2 = c1trans >= c2trans;
+            assert atC1 | atC2;
             return c1depth;
         }
 
