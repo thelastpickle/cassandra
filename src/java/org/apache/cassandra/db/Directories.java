f44c2015979a51b7253a8cee1fd6e3fb7ac41637
Jakub Żytka <jakub.zytka@datastax.com>
2022-02-14 08:53:27 +0100

    STAR-1120: make FileType assertion in Directories more useful (#340)
    (cherry picked from commit 79abd74276d54593c90f1ec1e2de0a2945e258b2)
    (cherry picked from commit 77acde06a8003d6dc5f0277043d1af0b3d5a5ab9)
    (cherry picked from commit 6d3a919b4004d94a4bbb94e14ab22317cc5a0811)
    (cherry picked from commit cd006abb9302b9f33ce8536e3890cdbb1fe1ec89)
    (cherry picked from commit e4f702ef39035301da544d605d4a682265f38ed0)

--- src/java/org/apache/cassandra/db/Directories.java
+++ src/java/org/apache/cassandra/db/Directories.java
@@ -1143,7 +1143,7 @@ public class Directories
                         return false;
 
                     default:
-                        throw new AssertionError();
+                        throw new AssertionError("unexpected file type: " + type + " for file " + file);
                 }
             };
         }
