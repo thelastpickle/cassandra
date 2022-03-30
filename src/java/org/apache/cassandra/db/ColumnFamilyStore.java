a21535d0e0883e62134bf210c062d65fadf0eb08
Ruslan Fomkin <Ruslan.Fomkin@gmail.com>
2022-03-30 19:43:34 +0200

    STAR-1231 Add BATCHLOG_REPLAY to ColumnFamilyStore.FlushReason (#398)
    Needed for CNDB
    
    (cherry picked from commit 4e81f0dc8356a39af637076ec39ae1dabbccf21a)
    (cherry picked from commit 9a765b1a9b26fba37fba9a24263d94e8bdd7fd73)
    (cherry picked from commit f8d2854a66f4f94198fd38f123159a6351ee277e)
    (cherry picked from commit 7a18abab3d55f49cb27b53103ab28000cfa5a808)
    (cherry picked from commit 45639340488c2d8c1d9f1f2c2fb3217e3009a53f)

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -257,7 +257,8 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
         OWNED_RANGES_CHANGE,
         UNIT_TESTS, // explicitly requested flush needed for a test
         /** Flush performed to a remote storage. Used by remote commit log replay */
-        REMOTE_REPLAY
+        REMOTE_REPLAY,
+        BATCHLOG_REPLAY
     }
 
     private static final String[] COUNTER_NAMES = new String[]{"table", "count", "error", "value"};
