9f98786d8c97716c342734cec7e257b4d82170f9
Piotr Kołaczkowski <pkolaczk@datastax.com>
2022-04-05 21:24:35 +0200

    STAR-1233: Use Guava concurrent hash set instead of Apache Mina
    Apache Mina is not really in the direct dependencies
    of the project. It compiled accidentally because it
    got onto the classpath through one of the
    transitive dependencies of Netty.
    
    (cherry picked from commit 3d3e2ee1cc1a057d1d69ba07815d015ef8f377e2)
    (cherry picked from commit 8cfc38220ac01719159cca801097e8f3dcd097c6)
    (cherry picked from commit 8ea374bd58af1d9bfa156b44560f05af8b79ed32)
    (cherry picked from commit dc47329721b9da0e8dd8eb75f0472fb8a4891766)
    (cherry picked from commit 8c9f28b3f3bbb77e78c02ac72e66d910f487fc67)

--- src/java/org/apache/cassandra/index/sai/StorageAttachedIndexGroup.java
+++ src/java/org/apache/cassandra/index/sai/StorageAttachedIndexGroup.java
@@ -31,6 +31,7 @@ import javax.annotation.concurrent.ThreadSafe;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
