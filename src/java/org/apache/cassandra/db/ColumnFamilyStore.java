59810798ac72937c5ca510a59a1141e730a9e698
Ruslan Fomkin <Ruslan.Fomkin@gmail.com>
2022-05-19 17:36:19 +0200

    STAR-1314 Use more CFS getters for CNDB mocked test (#449)
    Introduces an additional getter in ColumnFamilyStore and increases
    usage of its getters, so ColumnFamilyStore can be mocked in a CNDB
    test.
    
    (cherry picked from commit 04dbab025c11d782d5d335c2a96ca95fcbf65191)
    (cherry picked from commit d566637a2ff639b162a0d2653a21bc90aa690eed)
    (cherry picked from commit 0c4c84239a31799bf6983118655564fcb166115b)
    (cherry picked from commit de6933c5f9c264e35f6cf5b69e125f729027bed5)
    (cherry picked from commit 0ac6bf47bbd280bb210edece4e4f0b5bc431e11e)

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -141,6 +141,7 @@ import org.apache.cassandra.io.sstable.format.Version;
 import org.apache.cassandra.io.sstable.metadata.MetadataCollector;
 import org.apache.cassandra.io.util.File;
 import org.apache.cassandra.io.util.FileOutputStreamPlus;
+import org.apache.cassandra.metrics.KeyspaceMetrics;
 import org.apache.cassandra.metrics.Sampler;
 import org.apache.cassandra.metrics.Sampler.Sample;
 import org.apache.cassandra.metrics.Sampler.SamplerType;
@@ -1080,6 +1081,11 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
     public String getKeyspaceName()
     {
         return keyspace.getName();
+    }
+
+    public KeyspaceMetrics getKeyspaceMetrics()
+    {
+        return keyspace.metric;
     }
 
     @Override
