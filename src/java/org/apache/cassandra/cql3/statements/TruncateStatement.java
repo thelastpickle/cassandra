7d4d4e2c1c987c7892438384d302745a32594431
dan jatnieks <danj@datastax.com>
2022-07-21 07:30:13 -0700

    STAR-1505 doTruncateBlocking should be called from execute not executeLocally so it can be overridden by alternate implementations. (#487)
    (cherry picked from commit 6104667bf27f57992b5e96cbbffbd3c8aaada153)
    (cherry picked from commit 927f5f4b178f4b612885ea9a8ccb4bd3beeffcf4)
    (cherry picked from commit 13c1b8e38bd9c1b3f7e9cd7d8d5a3d7147a2d505)
    (cherry picked from commit 12a32e51b39583336090a98f307ca80eca428098)

--- src/java/org/apache/cassandra/cql3/statements/TruncateStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/TruncateStatement.java
@@ -94,6 +94,9 @@ public class TruncateStatement implements CQLStatement, CQLStatement.SingleKeysp
         try
         {
             TableMetadata metaData = Schema.instance.getTableMetadata(keyspace(), name());
+            if (metaData == null)
+                throw new InvalidRequestException(String.format("Unknown keyspace/table %s.%s", keyspace(), name()));
+
             if (metaData.isView())
                 throw new InvalidRequestException("Cannot TRUNCATE materialized view directly; must truncate base table instead");
 
@@ -103,10 +106,10 @@ public class TruncateStatement implements CQLStatement, CQLStatement.SingleKeysp
             }
             else
             {
-                StorageProxy.instance.truncateBlocking(keyspace(), name());
+                doTruncateBlocking();
             }
         }
-        catch (UnavailableException | TimeoutException e)
+        catch (UnavailableException | TimeoutException | InvalidRequestException e)
         {
             throw new TruncateException(e);
         }
@@ -118,6 +121,9 @@ public class TruncateStatement implements CQLStatement, CQLStatement.SingleKeysp
         try
         {
             TableMetadata metaData = Schema.instance.getTableMetadata(keyspace(), name());
+            if (metaData == null)
+                throw new InvalidRequestException(String.format("Unknown keyspace/table %s.%s", keyspace(), name()));
+            
             if (metaData.isView())
                 throw new InvalidRequestException("Cannot TRUNCATE materialized view directly; must truncate base table instead");
 
@@ -127,7 +133,8 @@ public class TruncateStatement implements CQLStatement, CQLStatement.SingleKeysp
             }
             else
             {
-                doTruncateBlocking();
+                ColumnFamilyStore cfs = Keyspace.open(keyspace()).getColumnFamilyStore(name());
+                cfs.truncateBlocking();
             }
         }
         catch (Exception e)
@@ -142,10 +149,9 @@ public class TruncateStatement implements CQLStatement, CQLStatement.SingleKeysp
         VirtualKeyspaceRegistry.instance.getTableNullable(id).truncate();
     }
 
-    protected void doTruncateBlocking()
+    protected void doTruncateBlocking() throws TimeoutException
     {
-        ColumnFamilyStore cfs = Keyspace.open(keyspace()).getColumnFamilyStore(name());
-        cfs.truncateBlocking();
+        StorageProxy.instance.truncateBlocking(keyspace(), name());
     }
 
     @Override
