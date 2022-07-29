5549214709ab15e91d7ba833a1a895833fe8937d
elliott.russo <elliott.russo@datastax.com>
2022-07-29 16:04:44 +0100

    STAR-1579: Add scaling parameter in the compaction log analysis report
    Addition to the compaction_report.html file which shows the
    W value changing over time graphically.
    
    In the drop down menu, select W value to see how W changes
    over your test (more useful for adaptive tests than
    static scaling parameter tests).
    
    Minor change to the graph.html file which automatically scales the negative
    y-axis of the compaction report
    
    The y-axis in the compaction report was fixed to a minimum value of 0.
    The addition of graphically representing the W value changing over time
    means having to scale the y-axis manually within compaction_report.html
    as the W parameter becomes negative in the read only stage.
    The graph.html change automatically scales the y-axis only if there is a
    negative data point (meaning it only really effects the W value plot).
    
    Final touches to CompactionLogAnalyzer and graph.html
    
    (cherry picked from commit a0d37a5444bcb9215bd842655ab60932fd5935c1)
    (cherry picked from commit 2f632778e97765ea98cfcdf2253eb765f1e1e4ba)
    (cherry picked from commit d503cb7d32ba8b6cf0c49b454ad9ac025dc51c5c)
    (cherry picked from commit 13f2576e74086bfd9e9040c1be94dfd1aab87642)

--- src/java/org/apache/cassandra/tools/CompactionLogAnalyzer.java
+++ src/java/org/apache/cassandra/tools/CompactionLogAnalyzer.java
@@ -116,6 +116,8 @@ public class CompactionLogAnalyzer
         int bucketsAboveT;
         // number of buckets above T*T sstables (excl compacting)
         int bucketsAboveT2;
+        // value of scaling parameter W
+        int scalingParameter;
 
         /**
          * Called to aggregate data in response to a new data point for a bucket.
@@ -135,6 +137,7 @@ public class CompactionLogAnalyzer
             remainingReadBytes += toAdd.remainingReadBytes - toRemove.remainingReadBytes;
             bucketsAboveT += toAdd.bucketsAboveT - toRemove.bucketsAboveT;
             bucketsAboveT2 += toAdd.bucketsAboveT2 - toRemove.bucketsAboveT2;
+            scalingParameter = toAdd.scalingParameter;
         }
     }
 
@@ -158,6 +161,7 @@ public class CompactionLogAnalyzer
     static int writePerSecIndex;
     static int sizesIndex;
     static int Tindex;
+    static int Windex;
 
     private static void initializeIndexes(String header)
     {
@@ -183,6 +187,7 @@ public class CompactionLogAnalyzer
                     sizesIndex = indexMap.getOrDefault("Tot/Read/Written", -1);
                     sizesIndex = indexMap.get("Tot. comp. size/Read/Written (bytes)");
                     Tindex = indexMap.get("T");
+                    Windex = indexMap.get("W");
                 }
             }
     }
@@ -206,6 +211,7 @@ public class CompactionLogAnalyzer
         dp.totalBytes = parseHumanReadable(sizes[0]);
         dp.remainingReadBytes = dp.totalBytes - parseHumanReadable(sizes[1]);
         int T = Integer.parseInt(data[Tindex]);
+        dp.scalingParameter = Integer.parseInt(data[Windex]);
         int compactingSSTables = Integer.parseInt(data[compactingSstablesIndex].split("/")[1]);
         int nonCompacting = dp.sstables - compactingSSTables;
         dp.bucketsAboveT = nonCompacting > T ? 1 : 0;
@@ -481,10 +487,10 @@ public class CompactionLogAnalyzer
         metrics.add("Remaining GB to compact");
         metrics.add("Number of buckets above T sstables");
         metrics.add("Number of buckets above T^2 sstables");
-
         metrics.add("Max SSTables in bucket");
         metrics.add("90th percentile SSTables in bucket");
         metrics.add("50th percentile SSTables in bucket");
+        metrics.add("Scaling parameter W");
 
         metrics.add("time");
         return metrics;
@@ -523,6 +529,8 @@ public class CompactionLogAnalyzer
         metrics.add(hist.max());
         metrics.add(hist.percentile(0.90));
         metrics.add(hist.percentile(0.50));
+
+        metrics.add(totals.scalingParameter);
 
         metrics.add((totals.timestamp - startTimestamp) / 1000.0);
         intervals.add(metrics);
