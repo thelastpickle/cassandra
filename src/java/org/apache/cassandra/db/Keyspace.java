69453705a33f1c34bb51e169dca52514082fe9c6
Jacek Lewandowski <6516951+jacek-lewandowski@users.noreply.github.com>
2022-08-23 21:10:20 +0200

    STAR-1602: Remove redundant waiting for read/write barrier when dropping a table (#514)
    (cherry picked from commit e70219627d692f186cf6bb6c77f6934a04c8c973)
    (cherry picked from commit ce5173cb2c3562ef5ee691b1d3b7b1d5aa7698bd)
    (cherry picked from commit 605fe06ab2934309f43261f617f774f09b62a019)
    (cherry picked from commit e049131d56ed8d99083b5398dd746d5ecd410ef0)

--- src/java/org/apache/cassandra/db/Keyspace.java
+++ src/java/org/apache/cassandra/db/Keyspace.java
@@ -389,10 +389,6 @@ public class Keyspace
 
         cfs.onTableDropped();
 
-        // wait for any outstanding reads/writes that might affect the CFS
-        writeOrder.awaitNewBarrier();
-        cfs.readOrdering.awaitNewBarrier();
-
         unloadCf(cfs, dropData);
     }
 
