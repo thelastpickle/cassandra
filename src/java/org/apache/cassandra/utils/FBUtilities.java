da92f8b445b04e5cde9107b50a888ae7669f49ba
Branimir Lambov <branimir.lambov@datastax.com>
2023-07-06 15:46:40 +0300

    Switch to uppercase K for kilo
    in line with what's used in other places in the OSS codebase
    
    (cherry picked from commit 50c4c16947864c39c4d8ec01f0523a1f5be2ad8f)
    (cherry picked from commit 718370bf957f564205c8bc248f06c33250dac85f)

--- src/java/org/apache/cassandra/utils/FBUtilities.java
+++ src/java/org/apache/cassandra/utils/FBUtilities.java
@@ -870,7 +870,8 @@ public class FBUtilities
     /**
      * Convert the given size in bytes to a human-readable value using binary (i.e. 2^10-based) modifiers.
      * For example, 1.000KiB, 2.100GiB etc., up to 8.000 EiB.
-     * @param size      Number to convert.
+     *
+     * @param size Number to convert.
      */
     public static String prettyPrintMemory(long size)
     {
