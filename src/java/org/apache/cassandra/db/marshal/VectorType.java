745ab63c6f2d91a28edd3e2da6ec6ac6d9ea8d91
Andrés de la Peña <adelapena@users.noreply.github.com>
2023-07-18 15:23:30 +0100

    VECTOR-68 Vector dimensions are not being validated correctly (#679)

--- src/java/org/apache/cassandra/db/marshal/VectorType.java
+++ src/java/org/apache/cassandra/db/marshal/VectorType.java
@@ -87,8 +87,6 @@ public final class VectorType<T> extends AbstractType<List<T>>
     private VectorType(AbstractType<T> elementType, int dimension)
     {
         super(ComparisonType.CUSTOM);
-        if (!(elementType instanceof FloatType))
-            throw new InvalidRequestException(String.format("vectors may only use float. given %s", elementType.asCQL3Type()));
         if (dimension <= 0)
             throw new InvalidRequestException(String.format("vectors may only have positive dimensions; given %d", dimension));
         this.elementType = elementType;
