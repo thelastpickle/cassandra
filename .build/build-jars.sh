b654e3c2289043f57e1867c701aba89517f96648
richamishra <richamishra614@gmail.com>
2023-09-13 16:14:31 +0530

    updated script

--- .build/build-jars.sh
+++ .build/build-jars.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -e -x
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
