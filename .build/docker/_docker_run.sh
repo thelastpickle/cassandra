56cafc923b7ff238ea304e6329b57dae8212d86e
richamishra <richamishra614@gmail.com>
2023-09-13 15:14:32 +0530

    updated docker script

--- .build/docker/_docker_run.sh
+++ .build/docker/_docker_run.sh
@@ -123,7 +123,7 @@ container_id=$(docker run --name ${container_name} -d --security-opt seccomp=unc
     ${image_name} sleep 1h)
 
 echo "Running container ${container_name} ${container_id}"
-
+sleep 600
 #docker exec --user root ${container_name} bash -c "\${CASSANDRA_DIR}/.build/docker/_create_user.sh build $(id -u) $(id -g)"
 docker exec --user root ${container_name} bash -c "/home/build/cassandra/.build/docker/_create_user.sh build $(id -u) $(id -g)"
 docker exec --user build ${container_name} bash -c "${docker_command}"
