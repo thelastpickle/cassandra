291c042524f2a531af8307c500633d13239066e8
richamishra <richamishra614@gmail.com>
2023-10-09 18:44:08 +0530

    commented artifacts to troubleshoot

--- .build/docker/run-tests.sh
+++ .build/docker/run-tests.sh
@@ -212,7 +212,8 @@ docker exec --user root ${container_name} update-alternatives --set python /usr/
 #docker exec --user cassandra ${container_name} bash -c "${docker_command}" | tee -a ${logfile}
 status=$?
 #docker cp ${container_name}:/home/cassandra/cassandra/build/test/output ${build_dir}/test
-docker cp ${container_name}:/home/cassandra/cassandra/build/build_docker_run.G62tsKBLrH /home/jenkins/agent/workspace/k8s-e2e/build
+mkdir -p /home/jenkins/agent/workspace/k8s-e2e/build/build_docker_run.G62tsKBLrH
+docker cp 71b682eb7efe:/home/cassandra/cassandra/build/. /home/jenkins/agent/workspace/k8s-e2e/build/build_docker_run.G62tsKBLrH
 #docker exec --user cassandra ${container_name} bash -c "mkdir -p /home/cassandra/cassandra/result/1/${target} && cp -r /home/cassandra/cassandra/build/test/output /home/cassandra/cassandra/result/1/${target}"
 if [ "$status" -ne 0 ] ; then
     echo "${docker_id} failed (${status}), debug…"
