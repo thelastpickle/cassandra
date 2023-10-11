eb02c2521e73ef49de943cc4a25300b8d78d28b0
Michael Marshall <michael.marshall@datastax.com>
2023-10-11 13:04:42 -0500

    Standardize not found logic for PrimaryKeyMap (#796)
    * Standardize not found logic for PrimaryKeyMap
    
    * Fix failing test; fix flipped names

--- src/java/org/apache/cassandra/index/sai/disk/v1/PartitionAwarePrimaryKeyMap.java
+++ src/java/org/apache/cassandra/index/sai/disk/v1/PartitionAwarePrimaryKeyMap.java
@@ -165,12 +165,6 @@ public class PartitionAwarePrimaryKeyMap implements PrimaryKeyMap
     }
 
     @Override
-    public boolean isNotFound(long sstableRowId)
-    {
-        return sstableRowId < 0;
-    }
-
-    @Override
     public void close() throws IOException
     {
         FileUtils.closeQuietly(rowIdToToken, rowIdToOffset, keyFetcher);
