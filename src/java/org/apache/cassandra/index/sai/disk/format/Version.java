ae550cf8b24c1a76856c332b7d150ba517c4f3f3
Andrey Yegorov <8622884+dlg99@users.noreply.github.com>
2024-02-02 13:45:23 -0800

    Vector does not support version less than BA (#966)

--- src/java/org/apache/cassandra/index/sai/disk/format/Version.java
+++ src/java/org/apache/cassandra/index/sai/disk/format/Version.java
@@ -48,6 +48,7 @@ public class Version
     public static final List<Version> ALL = Lists.newArrayList(CA, BA, AA);
 
     public static final Version EARLIEST = AA;
+    public static final Version VECTOR_EARLIEST = BA;
     // The latest version can be configured to be an earlier version to support partial upgrades that don't
     // write newer versions of the on-disk formats.
     public static final Version LATEST = parse(CassandraRelevantProperties.SAI_LATEST_VERSION.getString());
