c070b4d98b2773c4a5ada9344fc9c86e3fd3104b
Mick Semb Wever <mck@apache.org>
2024-03-11 15:45:18 +0100

    squash – fix for containerd uname being weird

--- test/unit/org/apache/cassandra/utils/FBUtilitiesTest.java
+++ test/unit/org/apache/cassandra/utils/FBUtilitiesTest.java
@@ -38,13 +38,14 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import com.google.common.primitives.Ints;
+import com.vdurmont.semver4j.Semver;
+import com.vdurmont.semver4j.SemverException;
 import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.vdurmont.semver4j.Semver;
 import org.apache.cassandra.config.Config;
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.db.marshal.AbstractType;
@@ -386,6 +387,10 @@ public class FBUtilitiesTest
 
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> parseKernelVersion("\n \n"))
                                                                  .withMessageContaining("no version found");
+
+        // containerd example (TODO we should actually handle this, despite it not being semver)
+        assertThatExceptionOfType(SemverException.class).isThrownBy(() -> parseKernelVersion("5.15.133+"))
+                                                                 .withMessageContaining("The build cannot be empty");
     }
 
     @Test
