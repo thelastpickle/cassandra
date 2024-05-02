909b5295b12539692d774452134c7b947fdfd1e9
Branimir Lambov <branimir.lambov@datastax.com>
2024-05-02 22:23:16 +0300

    Reverse cursor and iteration support (#1079)
    * STAR-1909: Reverse cursor and reverse iteration support for the trie interfaces
    
    * Combined forward and reverse iteration code in MemtableReadTrie

--- src/java/org/apache/cassandra/db/tries/MemtableTrie.java
+++ src/java/org/apache/cassandra/db/tries/MemtableTrie.java
@@ -829,7 +829,7 @@ public class MemtableTrie<T> extends MemtableReadTrie<T>
      */
     public <U> void apply(Trie<U> mutation, final UpsertTransformer<T, U> transformer) throws SpaceExhaustedException
     {
-        Cursor<U> mutationCursor = mutation.cursor();
+        Cursor<U> mutationCursor = mutation.cursor(Direction.FORWARD);
         assert mutationCursor.depth() == 0;
         ApplyState state = applyState;
         state.reset();
