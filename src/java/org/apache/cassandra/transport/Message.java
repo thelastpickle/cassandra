msmygit/vscassandra

STAR-1249 - Relax access to tracing methods in Message (#409)

src/java/org/apache/cassandra/transport/Message.java
@@ -253,7 +253,7 @@ public abstract class Message
             return response;
         }
 
-        void setTracingRequested()
+        public void setTracingRequested()
         {
             tracingRequested = true;
         }
@@ -283,7 +283,7 @@ public abstract class Message
             return this;
         }
 
-        UUID getTracingId()
+        public UUID getTracingId()
         {
             return tracingId;
         }

thelastpickle/cassandra

(cherry picked from commit 8ff3b81c6a1bee5c011fa4e684a71477de6790e3)
(cherry picked from commit d4850a710f2bbd3ecf46e103e17fc4a50afa0c2f)
(cherry picked from commit 2c9aa44ac9bfad0834b27e639902d65c35885699)
(cherry picked from commit dcf87040e3da9ba7cb0a78b7451f5cf9334461e6)
(cherry picked from commit f251c31a83f34ba0bba4076f7faa0ed6075ea59f)

src/java/org/apache/cassandra/transport/Message.java
@@ -267,7 +267,7 @@ public abstract class Message
             return response;
         }
 
-        void setTracingRequested()
+        public void setTracingRequested()
         {
             tracingRequested = true;
         }
@@ -297,7 +297,7 @@ public abstract class Message
             return this;
         }
 
-        TimeUUID getTracingId()
+        public TimeUUID getTracingId()
         {
             return tracingId;
         }