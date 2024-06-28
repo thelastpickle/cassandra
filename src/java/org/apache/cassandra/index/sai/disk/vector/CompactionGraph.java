6f4322f518f1da8056db7ffd570950428de308b9
Jonathan Ellis <jbellis@datastax.com>
2024-06-28 17:37:30 -0500

    give a name to the postings writer thread

--- src/java/org/apache/cassandra/index/sai/disk/vector/CompactionGraph.java
+++ src/java/org/apache/cassandra/index/sai/disk/vector/CompactionGraph.java
@@ -277,7 +277,7 @@ public class CompactionGraph implements Closeable, Accountable
             // write postings asynchronously while we run cleanup().  this requires the index header to be present
             writer.writeHeader();
             long postingsOffset = postingsOutput.getFilePointer();
-            var es = ExecutorFactory.Global.executorFactory().sequential("SAI-CompactionGraph");
+            var es = ExecutorFactory.Global.executorFactory().sequential("CompactionGraphPostingsWriter");
             var indexHandle = perIndexComponents.get(IndexComponentType.TERMS_DATA).createFileHandle();
             var index = OnDiskGraphIndex.load(indexHandle::createReader, termsOffset);
             var postingsFuture = es.submit(() -> {
