fdab62318e7558f37bded87cbf0e7ef88a3d433c
Daniel Jatnieks <jatnieks@pobox.com>
2024-07-01 17:11:12 -0700

    CC5 fix NativeTransportEncryptionOptionsTest.negotiatedProtocolMustBeAcceptedProtocolTest by removing TLSv1.1 from the test. As noted in CASSANDRA-18540, TLSv1.1 is disabled by default in JDK 8 and higher, and it's easier for CC to remove this protocol version than to modify jdk installations on the jenkins CI runners.

--- test/distributed/org/apache/cassandra/distributed/test/NativeTransportEncryptionOptionsTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/NativeTransportEncryptionOptionsTest.java
@@ -170,7 +170,7 @@ public class NativeTransportEncryptionOptionsTest extends AbstractEncryptionOpti
             c.set("client_encryption_options",
                   ImmutableMap.builder().putAll(validKeystore)
                               .put("enabled", true)
-                              .put("accepted_protocols", ImmutableList.of("TLSv1.1", "TLSv1.2", "TLSv1.3"))
+                              .put("accepted_protocols", ImmutableList.of("TLSv1.2", "TLSv1.3"))
                               .build());
         }).start())
         {
@@ -183,9 +183,9 @@ public class NativeTransportEncryptionOptionsTest extends AbstractEncryptionOpti
             tls10Connection.assertReceivedHandshakeException();
 
             TlsConnection tls11Connection = new TlsConnection(address.getHostAddress(), port, Collections.singletonList("TLSv1.1"));
-            Assert.assertEquals("Should be possible to establish a TLSv1.1 connection",
-                                ConnectResult.NEGOTIATED, tls11Connection.connect());
-            Assert.assertEquals("TLSv1.1", tls11Connection.lastProtocol());
+            Assert.assertEquals("Should not be possible to establish a TLSv1.1 connection",
+                                ConnectResult.FAILED_TO_NEGOTIATE, tls11Connection.connect());
+            tls11Connection.assertReceivedHandshakeException();
 
             TlsConnection tls12Connection = new TlsConnection(address.getHostAddress(), port, Collections.singletonList("TLSv1.2"));
             Assert.assertEquals("Should be possible to establish a TLSv1.2 connection",
