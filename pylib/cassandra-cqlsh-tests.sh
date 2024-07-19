5951087663cdcd7dcefe6fea5f3db9ddb9571439
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2024-07-19 08:32:01 +0200

    CNDB-10190: Fix CQLSH tests script

--- pylib/cassandra-cqlsh-tests.sh
+++ pylib/cassandra-cqlsh-tests.sh
@@ -23,35 +23,48 @@
 #
 ################################
 
+set -x
+
 WORKSPACE=$1
 
 [ "x${WORKSPACE}" != "x" ] || WORKSPACE="$(readlink -f $(dirname "$0")/..)"
 [ "x${BUILD_DIR}" != "x" ] || BUILD_DIR="${WORKSPACE}/build"
 [ "x${DIST_DIR}" != "x" ] || DIST_DIR="${WORKSPACE}/build"
+[ "x${TMPDIR}" != "x" ] || TMPDIR="$(mktemp -d ${DIST_DIR}/run-python-dtest.XXXXXX)"
 
-export TMPDIR="$(mktemp -d ${DIST_DIR}/run-python-dtest.XXXXXX)"
+mkdir -p "${TMPDIR}"
+export TMPDIR
 export PYTHONIOENCODING="utf-8"
 export PYTHONUNBUFFERED=true
 export CASS_DRIVER_NO_EXTENSIONS=true
-export CASS_DRIVER_NO_CYTHON=true
 export CCM_MAX_HEAP_SIZE="2048M"
 export CCM_HEAP_NEWSIZE="200M"
 export CCM_CONFIG_DIR="${TMPDIR}/.ccm"
 export NUM_TOKENS="16"
 export CASSANDRA_DIR=${WORKSPACE}
 
-java_version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | awk -F. '{print $1}')
-version=$(grep 'property\s*name=\"base.version\"' ${CASSANDRA_DIR}/build.xml |sed -ne 's/.*value=\"\([^"]*\)\".*/\1/p')
+if [[ -z "${version}" ]]; then
+  version=$(grep 'property\s*name=\"base.version\"' ${CASSANDRA_DIR}/build.xml |sed -ne 's/.*value=\"\([^"]*\)\".*/\1/p')
+fi
 
-python_version="3.8"
-command -v python3 >/dev/null 2>&1 && python_version="$(python3 -V | awk '{print $2}' | awk -F'.' '{print $1"."$2}')"
+if [[ -z "${java_version}" ]]; then
+  java_version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | sed 's/^1\.//g' | awk -F. '{print $1}')
+fi
+if [[ ${java_version} -ge 11 ]]; then
+  export CASSANDRA_USE_JDK11=true
+fi
+
+if [[ -z "${python_version}" ]]; then
+  python_version="3.8"
+  command -v python3 >/dev/null 2>&1 && python_version="$(python3 -V | awk '{print $2}' | awk -F'.' '{print $1"."$2}')"
+fi
 
 export TESTSUITE_NAME="cqlshlib.python${python_version}.jdk${java_version}"
 
 pushd ${CASSANDRA_DIR} >/dev/null
 
 # check project is already built. no cleaning is done, so jenkins unstash works, beware.
-[[ -f "${BUILD_DIR}/apache-cassandra-${version}.jar" ]] || [[ -f "${BUILD_DIR}/apache-cassandra-${version}-SNAPSHOT.jar" ]] || { echo "Project must be built first. Use \`ant jar\`. Build directory is ${BUILD_DIR} with: $(ls ${BUILD_DIR})"; exit 1; }
+[[ -f "${BUILD_DIR}/dse-db-${version}.jar" ]] || [[ -f "${BUILD_DIR}/dse-db-${version}-SNAPSHOT.jar" ]] || { echo "Project must be built first. Use \`ant jar\`. Build directory is ${BUILD_DIR} with: $(ls ${BUILD_DIR})"; exit 1; }
 
 # Set up venv with dtest dependencies
 set -e # enable immediate exit if venv setup fails
@@ -59,23 +72,33 @@ set -e # enable immediate exit if venv setup fails
 # fresh virtualenv and test logs results everytime
 rm -fr ${DIST_DIR}/venv ${DIST_DIR}/test/{html,output,logs}
 
+if [ "$cython" = "yes" ]; then
+  cython_suffix="cython"
+else
+  cython_suffix="no-cython"
+fi
+
 # re-use when possible the pre-installed virtualenv found in the cassandra-ubuntu2004_test docker image
-virtualenv-clone ${BUILD_HOME}/env${python_version} ${BUILD_DIR}/venv || virtualenv --python=python3 ${BUILD_DIR}/venv
+virtualenv-clone ${BUILD_HOME}/${cython_suffix}/python${python_version} ${BUILD_DIR}/venv || virtualenv --python=python${python_version} ${BUILD_DIR}/venv
 source ${BUILD_DIR}/venv/bin/activate
 
-pip install --exists-action w -r ${CASSANDRA_DIR}/pylib/requirements.txt
+if [ "$cython" = "yes" ]; then
+  pip install "Cython>=0.29.15,<3.0"
+  unset CASS_DRIVER_NO_CYTHON
+else
+  export CASS_DRIVER_NO_CYTHON=true
+fi
+
+pip install --exists-action s -r ${CASSANDRA_DIR}/pylib/requirements.txt
 pip freeze
 
 if [ "$cython" = "yes" ]; then
-    TESTSUITE_NAME="${TESTSUITE_NAME}.cython"
-    pip install "Cython>=0.29.15,<3.0"
     pushd pylib >/dev/null
     python setup.py build_ext --inplace
     popd >/dev/null
-else
-    TESTSUITE_NAME="${TESTSUITE_NAME}.no_cython"
 fi
-TESTSUITE_NAME="${TESTSUITE_NAME}.$(uname -m)"
+
+TESTSUITE_NAME="${TESTSUITE_NAME}.${cython_suffix}.$(uname -m)"
 
 ################################
 #
@@ -85,47 +108,30 @@ TESTSUITE_NAME="${TESTSUITE_NAME}.$(uname -m)"
 
 ccm remove test || true # in case an old ccm cluster is left behind
 ccm create test -n 1 --install-dir=${CASSANDRA_DIR}
-ccm updateconf "user_defined_functions_enabled: true"
+ccm updateconf "enable_user_defined_functions: true"
+ccm updateconf "cdc_enabled: true"
 
-version_from_build=$(ccm node1 versionfrombuild)
-export pre_or_post_cdc=$(python -c """from distutils.version import LooseVersion
-print (\"postcdc\" if LooseVersion(\"${version_from_build}\") >= \"3.8\" else \"precdc\")
-""")
-case "${pre_or_post_cdc}" in
-    postcdc)
-        ccm updateconf "cdc_enabled: true"
-        ;;
-    precdc)
-        :
-        ;;
-    *)
-        echo "${pre_or_post_cdc}" is an invalid value.
-        exit 1
-        ;;
-esac
+set +e # disable immediate exit from this point
 
-ccm start --wait-for-binary-proto
+ccm start --wait-for-binary-proto --jvm-version=${java_version}
 
 pushd ${CASSANDRA_DIR}/pylib/cqlshlib/ >/dev/null
 
-set +e # disable immediate exit from this point
 pytest --junitxml=${BUILD_DIR}/test/output/cqlshlib.xml
 RETURN="$?"
 
-# remove <testsuites> wrapping elements. `ant generate-unified-test-report` doesn't like it`
-sed -r "s/<[\/]?testsuites>//g" ${BUILD_DIR}/test/output/cqlshlib.xml > /tmp/cqlshlib.xml
-cat /tmp/cqlshlib.xml > ${BUILD_DIR}/test/output/cqlshlib.xml
-
 # don't do inline sed for linux+mac compat
-sed "s/testsuite name=\"pytest\"/testsuite name=\"${TESTSUITE_NAME}\"/g" ${BUILD_DIR}/test/output/cqlshlib.xml > /tmp/cqlshlib.xml
-cat /tmp/cqlshlib.xml > ${BUILD_DIR}/test/output/cqlshlib.xml
-sed "s/testcase classname=\"cqlshlib./testcase classname=\"${TESTSUITE_NAME}./g" ${BUILD_DIR}/test/output/cqlshlib.xml > /tmp/cqlshlib.xml
-cat /tmp/cqlshlib.xml > ${BUILD_DIR}/test/output/cqlshlib.xml
+sed "s/testsuite name=\"pytest\"/testsuite name=\"${TESTSUITE_NAME}\"/g" ${BUILD_DIR}/test/output/cqlshlib.xml > /${TMPDIR}/cqlshlib.xml
+cat /${TMPDIR}/cqlshlib.xml > ${BUILD_DIR}/test/output/cqlshlib.xml
+sed "s/testcase classname=\"cqlshlib./testcase classname=\"${TESTSUITE_NAME}./g" ${BUILD_DIR}/test/output/cqlshlib.xml > /${TMPDIR}/cqlshlib.xml
+cat /${TMPDIR}/cqlshlib.xml > ${BUILD_DIR}/test/output/cqlshlib.xml
 
 # tar up any ccm logs for easy retrieval
-if ls ${TMPDIR}/test/*/logs/* &>/dev/null ; then
+if ls ${TMPDIR}/.ccm/test/*/logs/* &>/dev/null ; then
     mkdir -p ${DIST_DIR}/test/logs
-    tar -C ${TMPDIR} -cJf ${DIST_DIR}/test/logs/ccm_logs.tar.xz */test/*/logs/*
+    pushd ${TMPDIR}/.ccm/test >/dev/null
+    tar -C ${TMPDIR}/.ccm/test -cJf ${DIST_DIR}/test/logs/ccm_logs.tar.xz */logs/*
+    popd >/dev/null
 fi
 
 ccm remove
@@ -135,7 +141,6 @@ ccm remove
 # Clean
 #
 ################################
-
 
 rm -rf ${TMPDIR}
 unset TMPDIR
