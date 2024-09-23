8e2fca39103f931dca92982fa6a4941f79b59592
Piotr Kołaczkowski <pkolaczk@gmail.com>
2024-09-23 14:28:56 +0200

    Update src/java/org/apache/cassandra/db/filter/RowFilter.java
    Fix typo in the comment
    
    Co-authored-by: Andrés de la Peña <adelapena@users.noreply.github.com>

--- src/java/org/apache/cassandra/db/filter/RowFilter.java
+++ src/java/org/apache/cassandra/db/filter/RowFilter.java
@@ -328,7 +328,7 @@ public abstract class RowFilter implements Iterable<RowFilter.Expression>
         {
             if (current.isDisjunction)
             {
-                // If we're in discujnction mode, we must not pass the current builder to addToRowFilter.
+                // If we're in disjunction mode, we must not pass the current builder to addToRowFilter.
                 // We create a new conjunction sub-builder instead and add all expressions there.
                 var builder = new Builder();
                 addToRowFilterDelegate.accept(builder);
